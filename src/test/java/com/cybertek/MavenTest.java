6be10a0ec7fed3b56b5256865874ca23f1418c38
rs8953365 <rs8953363@gmail.com>
2021-11-20 23:34:30 -0600

    ...

--- src/test/java/com/cybertek/MavenTest.java
+++ src/test/java/com/cybertek/MavenTest.java
@@ -1,6 +1,11 @@
 package com.cybertek;
 
 import com.github.javafaker.Faker;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MavenTest {
     public static void main(String[] args) {
@@ -8,5 +13,10 @@ public class MavenTest {
         System.out.println(faker.address().city());
         System.out.println(faker.name().name());
         System.out.println(faker.phoneNumber().cellPhone());
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+        driver.findElement(By.name("q")).sendKeys((faker.name().title()+ Keys.ENTER));
     }
 }
